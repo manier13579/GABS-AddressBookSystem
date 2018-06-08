c4204cc9d1161c30e77241ed2e2bf897abdf634c
Bing Zhe <9061@163.com>
2018-06-08 19:55:25 +0800

    test

--- sql/db.sql
+++ sql/db.sql
@@ -1,18 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost
-Source Server Version : 50711
-Source Host           : localhost:3306
-Source Database       : list3
-
-Target Server Type    : MYSQL
-Target Server Version : 50711
-File Encoding         : 65001
-
-Date: 2018-06-07 12:03:51
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------

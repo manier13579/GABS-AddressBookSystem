d185dbb5247b6aeac58dbe2ff40d238f7b77030a
Bing Zhe <9061@163.com>
2018-06-12 08:23:03 +0800

    增加注释

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,12 @@
 <?php
 require_once dirname(__FILE__).'/../src/common/path.php';
 
+//登录测试
 require_once $rootpath.'/tests/signin_test.php';
+//首页测试
 require_once $rootpath.'/tests/list_test.php';
+
+
 
 
 ?>
